4a6fbbe393f8cd1ab044275d9dab0a4624cbe988
ZmasulinnI <v.poroshin1604@yandex.ru>
2024-01-06 03:04:23 +0300

    hello!

---
+++ lecar_app/database/_Con.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lecar_app.database;
+
+public partial class Con
+{
+    public int IdIllnes { get; set; }
+
+    public int IdMedicament { get; set; }
+}// dsfsdf
